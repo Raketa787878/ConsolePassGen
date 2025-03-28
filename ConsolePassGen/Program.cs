43c47ddf72395d834f0592722090dd0d0c82d773
Ivan <168999247+Raketa787878@users.noreply.github.com>
2025-03-28 20:06:08 +0300

    Добавьте файлы проекта.

---
+++ ConsolePassGen/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsolePassGen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter length: ");
+            int length = Convert.ToInt32(Console.ReadLine());
+            
+        }
+    }
+}
