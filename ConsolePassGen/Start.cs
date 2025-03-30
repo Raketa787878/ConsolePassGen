a7b57225d461bb76f93e590159b64babfdab0b0f
Ivan <168999247+Raketa787878@users.noreply.github.com>
2025-03-30 21:38:50 +0300

    Added check password length, and added saving of the generated password.

---
+++ ConsolePassGen/Start.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsolePassGen
+{
+    class Start
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("1. Generate Password");
+                Console.WriteLine("2. Save Password in file");
+                Console.WriteLine("3. Exit");
+
+                byte choice = Convert.ToByte(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        
+                        GeneratePassword.PasswordSettings();
+                        break;
+
+                    case 2:
+                        SavePassword.Save();
+                        break;
+
+                    case 3:
+                        return;
+                }
+            }
+        }
+    }
+}
